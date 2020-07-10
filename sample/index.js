94a3059c6fbaeaff635ec06b9ce3089fa7ac925a
uptonking <jinyaoo86@gmail.com>
2020-07-10 21:52:31 +0800

    chore: css table demo

--- sample/index.js
+++ sample/index.js
@@ -1,3 +1,10 @@
+// basic
+export { default as A0Simple } from './simple/A0Simple';
 export { default as A1Basic } from './simple/A1Basic';
 export { default as A2Footer } from './simple/A2Footer';
 export { default as A3Sorting } from './simple/A3Sorting';
+
+// table
+
+export { default as HtmlTable } from './table/HtmlTable';
+export { default as DisplayTable } from './table/DisplayTable';
---
+++ sample/table/HtmlTable.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HtmlTable(props) {
+  return (
+    <div>
+      <h1>标题</h1>
+      content
+    </div>
+  );
+}
+
+export default HtmlTable;
